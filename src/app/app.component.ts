a6489aad2910e9c2965449639c0f0c7afe190cfc
bkfwebdev <bryant.feld@gmail.com>
2017-08-16 16:54:25 -0400

    initial set up and test

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component} from "@angular/core";
+
+export class AppComponent ({
+    selector: 'app-root',
+    template: '<h2> Hello World! </h2>',
+    styles: [ 'h2 { font-family: sans-serif; font-size: 1.2em; }' ],
+})
